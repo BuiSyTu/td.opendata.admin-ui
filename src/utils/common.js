2b542889df6ec719d771bfb9c7ec3bcfccaaa6be
Bui Sy Tu <tubs@tandan.com.vn>
2022-03-29 10:42:31 +0700

    feat: remove unused function

--- src/utils/common.js
+++ src/utils/common.js
@@ -2,12 +2,3 @@ export const toObject = (arr, key, value) => arr.reduce((previous, item) => ({
   ...previous,
   ...(item !== null && item !== undefined && { [item[key]]: item[value] }),
 }), {})
-
-export const tryParseStringToObject = (text, callback) => {
-  try {
-    const obj = JSON.parse(text)
-    return callback(obj)
-  } catch {
-    return false
-  }
-}
